77d79e9252da60b4690966d3bffa59e5833a7730
SthefanycPereira <sthefanydecarvalhopereira@gmail.com>
2026-09-23 01:22:40 +0000

    aula2209

--- projetoum/src/app/app.ts
+++ projetoum/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,5 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
 
+  cabecalhoLoja = "Loja Angular";
 }
